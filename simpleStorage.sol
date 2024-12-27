da5c26296dbe1b987c6b88464eae1aea1e36ce71
KaiAdrian <thriller30@gmail.com>
2024-12-27 08:56:47 +0100

    več funkcij

--- simpleStorage.sol
+++ simpleStorage.sol
@@ -2,5 +2,14 @@
 pragma solidity ^0.8.18; //solidity version
 
 contract simplStorage {
-    uint256 favNumber;// 0 by defu..
+    uint256  favNumber;// 0 by defu..
+
+    uint256[]seznamFavNumber;
+
+    function store(uint256 _favNumber)public{
+        favNumber =_favNumber;
+    }
+    function retrive() public view returns(uint256)  {
+        return favNumber;
+    }
 }
