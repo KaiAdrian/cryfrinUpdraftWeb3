a56cea5659357d11cfeb182d9c6ab60f13fcfac1
KaiAdrian <thriller30@gmail.com>
2025-01-11 19:57:00 +0100

    sfStore funct !FINISHED

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -5,12 +5,16 @@ pragma solidity ^0.8.18;
 import "./SimpleStorage.sol"; // import {SimpleStorage} from "./SimpleStorage.sol";
 
 contract StorageFactory {
-
-SimpleStorage public simpleStorage;
+  SimpleStorage[] public listOfsimpleStorageContracts;
 
   function createSimpleStorageContract() public  {
-    simpleStorage = new SimpleStorage();
+    SimpleStorage newSimpleStorageContract = new SimpleStorage();
+    listOfsimpleStorageContracts.push(newSimpleStorageContract);
 
+  }
+
+  function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+    //
   }
 }
 
