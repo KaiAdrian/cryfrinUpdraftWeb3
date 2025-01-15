e3fd1bff99938d2f0b645f36fd8f02c7985e983d
KaiAdrian <thriller30@gmail.com>
2025-01-15 20:31:54 +0100

    sfStore $ sfGet complete

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -10,11 +10,17 @@ contract StorageFactory {
   function createSimpleStorageContract() public  {
     SimpleStorage newSimpleStorageContract = new SimpleStorage();
     listOfsimpleStorageContracts.push(newSimpleStorageContract);
-
   }
 
   function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
-    //
+    
+   // SimpleStorage mySimpleStorage =listOfsimpleStorageContracts[_simpleStorageIndex];
+    listOfsimpleStorageContracts[_simpleStorageIndex].store(_newSimpleStorageNumber);
+  }
+
+  function sfGet (uint256 _simpleStorageIndex) public view  returns (uint256 ) {
+    //SimpleStorage mySimpleStorage = listOfsimpleStorageContracts[_simpleStorageIndex];
+    return listOfsimpleStorageContracts[_simpleStorageIndex].retrive();
   }
 }
 
