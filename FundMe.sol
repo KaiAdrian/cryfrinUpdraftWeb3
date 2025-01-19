45b938fb46d2581ae73b9f96e4cacaa384c5f057
KaiAdrian <thriller30@gmail.com>
2025-01-19 09:01:41 +0100

    fund() //furthered

--- FundMe.sol
+++ FundMe.sol
@@ -6,9 +6,11 @@
 pragma solidity ^0.8.18;
 
 contract FundMe {
+  uint256 public myValue=1;
 
-    function fund() public  {
-        
+    function fund() public payable  {
+        myValue=myValue+1;
+        require(msg.value> 1e18,"Amount sent is to low!"); //1 ETH
     }
     function withdraw() public  {
         
