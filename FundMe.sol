50694803c0c97e31250f845979dbe4833a1bc539
KaiAdrian <thriller30@gmail.com>
2025-01-22 13:33:14 +0100

    getPrice() GetConcersionRate()..

--- FundMe.sol
+++ FundMe.sol
@@ -4,6 +4,7 @@
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
+import  {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
   uint256 public minUsd = 5;
@@ -15,6 +16,9 @@ contract FundMe {
     function withdraw() public  {
         
     }
-
+function getPrice() public  {
+    //adress:0x694AA1769357215DE4FAC081bf1f309aDC325306
+}
+function getConversionRate() public  {}
 
 }
