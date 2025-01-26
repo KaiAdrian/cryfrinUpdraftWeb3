bd29e469630d4a5088fc156ee8e581c2e7e691fe
KaiAdrian <thriller30@gmail.com>
2025-01-26 11:04:30 +0100

    fund() ,getLatestPrice()

--- FundMe.sol
+++ FundMe.sol
@@ -1,9 +1,12 @@
+// SPDX-License-Identifier: MIT
+
 // get funds 
 // widr...
 // set min amount req
 
-// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.18;
+
 import  {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
@@ -13,12 +16,23 @@ contract FundMe {
         minUsd = minUsd + 1;
         require(msg.value >= minUsd,"Amount sent is to low!"); //1 ETH
     }
-    function withdraw() public  {
-        
-    }
-function getPrice() public  {
-    //adress:0x694AA1769357215DE4FAC081bf1f309aDC325306
-}
-function getConversionRate() public  {}
+   
+    function getLatestPrice() public view returns (uint256) {
+      AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
 
+      (,int answer,,,) = priceFeed.latestRoundData();
+
+      return uint(answer) * 1e10;
+}
+         
+   function getConversionRate() public  {}
+
+   function getVersion () public view returns (uint256) {
+
+    return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+   }
+
+  // function withdraw() public  {
+        
+    // }
 }
